02084338f76ffb83176492d31d40c7822fef438d
jon1267 <jonleev@gmail.com>
2019-07-03 14:04:29 +0300

    create DataController && routes

---
+++ app/Http/Controllers/AmoDataController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AmoDataController extends Controller
+{
+    public function create()
+    {
+
+    }
+
+    public function upload()
+    {
+
+    }
+
+
+}
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/create', 'AmoDataController@create')->name('amo.create');
+
+Route::get('/upload', 'AmoDataController@upload')->name('amo.upload');
